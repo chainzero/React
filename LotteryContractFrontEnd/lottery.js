eb5ecdc62864ec1b77a743d560586c52b5f57f66
Scott Carruthers <Iilluminorowebdev@gmail.com>
2021-04-20 11:15:32 -0400

    Adding source code for lottery smart contract react front end

---
+++ LotteryContractFrontEnd/lottery.js
@@ -0,0 +1,7 @@
+import web3 from "./web3"
+
+const address = "0xcA4dD09c72510e7132F784a77771d442e9866eC9"
+
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
+
+export default new web3.eth.Contract(abi, address)
